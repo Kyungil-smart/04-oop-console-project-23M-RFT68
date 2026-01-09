05fd73b4aa92dc337a9394ab50de1ca81db0e079
23M-RFT68 <kobh0221@gmail.com>
2026-01-09 17:03:02 +0900

    Create MiniGame Folder & Commit

---
+++ 미니게임 만들기/Managers/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class GameManager
+{
+    public static bool IsGameOver { get; set; }
+    public const string GameName = "";
+    private PlayerCharacter _Player;
+    
+    Public void Run()
+    {
+        Init();
+
+        while (!IsGameOver)
+        {
+            // 랜더링
+            Console.Clear(); // 콘솔창 비우기
+            SceneManager.Render();
+            InputManager.GetUserInput(); // 키입력 받기
+
+            if (InputManager.GetKey(ConsoleKey.Tab))
+            {
+                SceneManager.Change("Log");
+            }
+            // 데이터 처리
+            SceneManager.Update();
+        }
+    }
+
+    private void Init()
+    {
+        IsGameOver = false;
+        SceneManager.OnChangeScene 
+        
+        
+    }
+
+
+
+
+
+}
---
+++ 미니게임 만들기/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using _4차_과제_미니게임;
+
+class Program
+{
+    static void Main()
+    {
+        GameManager gamemanager = new GameManager();
+        gamemanager.Run();
+    }
+}
