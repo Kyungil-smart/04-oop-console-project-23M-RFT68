6fc85bdcbd829286245fab654c55b39f6b5a7c2a
23M-RFT68 <kobh0221@gmail.com>
2026-01-11 16:22:43 +0900

    Util upadate

---
+++ 미니게임 만들기/Scenes/CreditScene.cs
@@ -0,0 +1,52 @@
+﻿public class CreditScene : Scene
+{
+    private CreditList _credit;
+
+    public CreditScene()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        _credit = new CreditList();
+        _credit.Add("도움을 주신분 : ");
+        _credit.Add("만든이 : ");
+        _credit.Add("뒤로",);
+    }
+
+    public override void Enter()
+    {
+        _titleMenu.Reset();
+    }
+
+    public override void Update()
+    {
+        if (InputManager.GetKey(ConsoleKey.UpArrow))
+        {
+            _credit.SelectUp();
+        }
+
+        if (InputManager.GetKey(ConsoleKey.DownArrow))
+        {
+            _credit.SelectDown();
+        }
+
+        if (InputManager.GetKey(ConsoleKey.Escape))
+        {
+            _credit.Select();
+        }
+    }
+
+    public override void Render()
+    {
+        Console.SetCursorPosition(5, 1);
+        GameManager.
+        
+    }
+
+
+
+
+
+}
---
+++ 미니게임 만들기/Utils/MenuList.cs
@@ -0,0 +1,121 @@
+﻿
+public class MenuList
+{
+    private List<(string text, Action action)> _menus;
+    private int _currentIndex;
+    public int CurrentIndex { get => _currentIndex; }
+    private Ractangle _outLine;
+    private int _maxLength;
+
+    public MenuList(params (string, Action)[] menuTexts)
+    {
+        if (menuTexts == 0)
+        {
+            _menus = new List<(string, Action)>();
+        }
+        else
+        {
+            _menus = menuTexts.ToList();
+        }
+
+        for (int i = 0; i < _menus.Count; i++)
+        {
+            int textWidth = _menus[i].text.GetTextWidth();
+
+            if (_maxLength < textWidth)
+            {
+                _maxLength = textWidth;
+            }
+        }
+        _outLine = new Ractangle(width: _maxLength + 4, _menus.Count + 2);
+    }
+
+    public void Reset()
+    {
+        _currentIndex = 0;
+    }
+
+    public void Select()
+    {
+        if (_menus.Count == 0) return;
+        
+        _menus[_currentIndex].action?.Invoke();
+        
+        if (_currentIndex == 0) _currentIndex = 0;
+        else if (_currentIndex >= _menus.Count) _currentIndex = _menus.Count - 1;
+    }
+
+    public void Add(string text, Action action)
+    {
+        _menus.Add((text, action));
+
+        int textWidth = text.GetTextWidth();
+        if (_maxLength < textWidth)
+        {
+            _maxLength = textWidth; 
+        }
+        
+        _outLine.Width = _maxLength + 6;
+        _outLine.Height++;
+    }
+
+    public void Remove()
+    {
+        _menus.RemoveAt(_currentIndex);
+
+        int max = 0;
+
+        foreach ((string text, Action action) in _menus)
+        {
+            int textWidth = text.GetTextWidth();
+            
+            if (max < textWidth) max = textWidth;
+        }
+        
+        if (_maxLength != max) _maxLength = max;
+        
+        _outLine.Width = _maxLength + 6;
+        _outLine.Heigth--;
+    }
+
+    public void SelectUp()
+    {
+        _currentIndex--;
+
+        if (_currentIndex < 0)
+            _currentIndex = 0;
+    }
+
+    public void SelectDown()
+    {
+        _currentIndex++;
+        
+        if(_currentIndex >= _menus.Count) 
+            _currentIndex = _menus.Count - 1;
+    }
+    
+    public void Render(int x, int y)
+    {
+        _outLine.X = x;
+        _outLine.Y = y;
+        _outLine.Draw();
+
+        for (int i = 0; i < _menus.Count; i++)
+        {
+            y++;
+            Console.SetCursorPosition(x + 1, y);
+
+            if (i == _currentIndex)
+            {
+                "->".Print(ConsoleColor.White);
+                _menus[i].text.Print(ConsoleColor.White);
+                continue;
+            }
+            else
+            {
+                Console.Write("  ");
+                _menus[i].text.Print();
+            }
+        }
+    }
+}
